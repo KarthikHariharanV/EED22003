cd3e047e49d242412267c7c2272eafd8f45fba6c
Karthik Hariharan <164728782+KarthikHariharanV@users.noreply.github.com>
2024-04-01 22:32:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,39 +52,26 @@ document.addEventListener('keydown', function(event) {
 let myDocument = document.documentElement;
 let btn = document.getElementById("btn");
 
-btn.addEventListener("click", ()=>{
-    if(btn.textContent == "Go Fullscreen"){
-        if (myDocument.requestFullscreen) {
-            myDocument.requestFullscreen();
-        } 
-        else if (myDocument.msRequestFullscreen) {
-            myDocument.msRequestFullscreen();
-        } 
-        else if (myDocument.mozRequestFullScreen) {
-            myDocument.mozRequestFullScreen();
-        }
-        else if(myDocument.webkitRequestFullscreen) {
-            myDocument.webkitRequestFullscreen();
-        }
-        btn.textContent = "Exit Fullscreen";
-    }
-    else{
-        if(document.exitFullscreen) {
-            document.exitFullscreen();
-        }
-        else if(document.msexitFullscreen) {
-            document.msexitFullscreen();
-        }
-        else if(document.mozexitFullscreen) {
-            document.mozexitFullscreen();
-        }
-        else if(document.webkitexitFullscreen) {
-            document.webkitexitFullscreen();
-        }
-
-        btn.textContent = "Go Fullscreen";
-      }
-});
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
 function changeImage() {
   var image = document.getElementById('myImage');
   
